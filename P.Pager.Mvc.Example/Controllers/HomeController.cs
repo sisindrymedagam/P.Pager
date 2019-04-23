dbd1b99ec7e35619583b8bf2b4b4a54467cd46fc
sisindrymedagam <sisindrymedagam@hotmail.com>
2019-04-24 00:22:52 +0530

    Example is added

---
+++ P.Pager.Mvc.Example/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using P.Pager.Mvc.Example.Models;
+using System.Web.Mvc;
+
+namespace P.Pager.Mvc.Example.Controllers
+{
+    public class HomeController : Controller
+    {
+        readonly DemoData _data;
+        public HomeController()
+        {
+            _data = new DemoData();
+        }
+
+        public ActionResult Index(int page = 1)
+        {
+            var pager = _data.GetMembers().ToPagerList(page, 7);
+            return View(pager);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ P.Pager.Mvc.Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="P.Pager.Mvc.Example.MvcApplication" Language="C#" %>
